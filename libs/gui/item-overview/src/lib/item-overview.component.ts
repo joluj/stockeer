0702390800c9da77737010d82ff3a67a62c76e67
thorb <kopytotobias@gmail.com>
2022-04-15 21:09:15 +0200

    add basic list items to item-overview

--- libs/gui/item-overview/src/lib/item-overview.component.ts
+++ libs/gui/item-overview/src/lib/item-overview.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'stockeer-item-overview',
   templateUrl: './item-overview.component.html',
   styleUrls: ['./item-overview.component.scss'],
 })
-export class ItemOverviewComponent {}
+export class ItemOverviewComponent {
+  @Input()
+  items: string[];
+
+  @Output()
+  delete: EventEmitter<boolean>;
+
+  constructor() {
+    this.items = ['Mango', 'Johannes'];
+    this.delete = new EventEmitter();
+  }
+
+  onDeleteClick() {
+    this.delete.emit(true);
+  }
+}
