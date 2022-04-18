b3ac480a4a9d61d751d6ba48c17d2d69eaac72f2
Johannes Jablonski <johannes.lukas.jablonski@gmail.com>
2022-04-18 12:49:55 +0200

    :sparkles: Add Example store showcase (not intended for a real use case)

--- apps/app/src/app/app.component.spec.ts
+++ apps/app/src/app/app.component.spec.ts
@@ -2,11 +2,13 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { TestBed, waitForAsync } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
+import { AppStoreModule } from '@stockeer/store';
 
 describe('AppComponent', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [AppComponent],
+      imports: [AppStoreModule],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
   }));
@@ -16,5 +18,4 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
-  // TODO: add more tests!
 });
