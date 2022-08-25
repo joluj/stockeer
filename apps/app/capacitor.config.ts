4dc88ff4d32bf109913c7dc4173d2381edf70f29
Johannes Jablonski <johannes.lukas.jablonski@gmail.com>
2022-08-25 19:54:00 +0200

    Add hotswap to android app

--- apps/app/capacitor.config.ts
+++ apps/app/capacitor.config.ts
@@ -1,10 +1,33 @@
 import { CapacitorConfig } from '@capacitor/cli';
+import * as os from 'os';
+
+const url =
+  process.env['DEV_SERVER_URL'] ??
+  (() => {
+    // Just some hacky script that chooses the first 'valid' ip.
+    // Not really robust since it can be overwritten via env var.
+    const networks = os.networkInterfaces();
+    for (const network of Object.values(networks)) {
+      if (!network) continue;
+      const address = network[0]?.address ?? '';
+      if (address != '127.0.0.1') return `http://${address ?? ''}:4498/`;
+    }
+    return '';
+  })();
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'app',
   webDir: '../../dist/apps/app',
   bundledWebRuntime: false,
+  server: {
+    url,
+    cleartext: true,
+  },
 };
+
+if (process.env['BUILD_PRODUCTION']) {
+  delete config.server;
+}
 
 export default config;
