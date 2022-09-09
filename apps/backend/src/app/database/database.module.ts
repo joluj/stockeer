1cd11c7881377ccefbb8646e3828315cbc7c2286
Johannes Jablonski <johannes.lukas.jablonski@gmail.com>
2022-09-09 21:33:48 +0200

    Reset DB migrations

--- apps/backend/src/app/database/database.module.ts
+++ apps/backend/src/app/database/database.module.ts
@@ -2,8 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { environment } from '../../environments/environment';
 import { ProductEntity, StorageEntity, UserEntity } from '@stockeer/entities';
-import { InitDB1650362930944 } from './migrations/1650362930944-InitDB';
-import { StockeerAndProductEntities1662750499394 } from './migrations/1662750499394-StockeerAndProductEntities';
+import { Init1662751734633 } from './migrations/1662751734633-Init';
 
 @Module({
   imports: [
@@ -18,10 +17,7 @@ import { StockeerAndProductEntities1662750499394 } from './migrations/1662750499
         database: environment.database.database,
         entities: [UserEntity, StorageEntity, ProductEntity],
         synchronize: environment.database.synchronize,
-        migrations: [
-          InitDB1650362930944,
-          StockeerAndProductEntities1662750499394,
-        ],
+        migrations: [Init1662751734633],
         migrationsRun: environment.database.runMigrations,
       }
     ),
