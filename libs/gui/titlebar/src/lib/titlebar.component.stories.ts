303f284186962fcb2f64da7b1e9fdcea919b2f61
Johannes Jablonski <johannes.lukas.jablonski@gmail.com>
2022-10-14 18:59:50 +0200

    Add multiple stockeers to titlebar

--- libs/gui/titlebar/src/lib/titlebar.component.stories.ts
+++ libs/gui/titlebar/src/lib/titlebar.component.stories.ts
@@ -2,6 +2,8 @@ import { moduleMetadata, Story, Meta } from '@storybook/angular';
 import { TitlebarComponent } from './titlebar.component';
 import { IonicModule } from '@ionic/angular';
 import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
+import { Stockeer } from '@stockeer/store';
 
 export default {
   title: 'TitlebarComponent',
@@ -17,5 +19,32 @@ const Template: Story<TitlebarComponent> = (args: TitlebarComponent) => ({
   props: args,
 });
 
-export const Primary = Template.bind({});
-Primary.args = {};
+const stockeer1: Stockeer = new Stockeer({
+  id: 'test-id-1',
+  products: [],
+  name: 'Kitchen',
+});
+
+const stockeer2: Stockeer = new Stockeer({
+  id: 'test-id-1',
+  products: [],
+  name: 'Wine Refrigerator',
+});
+
+export const NoneSelected = Template.bind({});
+NoneSelected.args = {
+  stockeers$: of([stockeer1, stockeer2]),
+  selection$: of([]),
+} as Partial<TitlebarComponent>;
+
+export const OneSelected = Template.bind({});
+OneSelected.args = {
+  stockeers$: of([stockeer1, stockeer2]),
+  selection$: of([stockeer1]),
+} as Partial<TitlebarComponent>;
+
+export const TwoSelected = Template.bind({});
+TwoSelected.args = {
+  stockeers$: of([stockeer1, stockeer2]),
+  selection$: of([stockeer1, stockeer2]),
+} as Partial<TitlebarComponent>;
--- libs/gui/titlebar/src/lib/titlebar.component.ts
+++ libs/gui/titlebar/src/lib/titlebar.component.ts
@@ -1,4 +1,6 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { EMPTY, Observable } from 'rxjs';
+import { Stockeer } from '@stockeer/store';
 
 @Component({
   selector: 'stockeer-titlebar',
@@ -6,6 +8,12 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./titlebar.component.scss'],
 })
 export class TitlebarComponent {
+  @Input()
+  stockeers$: Observable<Stockeer[]>;
+
+  @Input()
+  selection$: Observable<Stockeer[]>;
+
   @Output()
   logout: EventEmitter<void>;
 
@@ -14,9 +22,15 @@ export class TitlebarComponent {
     // Reason: https://github.com/storybookjs/storybook/issues/17004
     // TLDR: storybook bug
     this.logout = new EventEmitter();
+    this.stockeers$ = EMPTY;
+    this.selection$ = EMPTY;
   }
 
   triggerLogout() {
     this.logout.emit();
+  }
+
+  compare(a: Stockeer, b: Stockeer) {
+    return a.id === b.id;
   }
 }
